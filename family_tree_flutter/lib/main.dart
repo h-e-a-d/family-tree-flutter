d72abfdf8e14e28b4a394b371f32670389b8bf51
h-e-a-d <head.name@gmail.com>
2025-06-04 09:08:39 +0000

    1

---
+++ family_tree_flutter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'views/home_view.dart';
+
+void main() {
+  runApp(FamilyTreeApp());
+}
+
+class FamilyTreeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Family Tree Builder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Arial',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeView(),
+    );
+  }
+}
